67059e099fd118eb2dcf4a7440d21f106d08a713
1alena123 <44710795+1alena123@users.noreply.github.com>
2019-05-21 02:35:43 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AppWindow appWindow = new AppWindow();
+        appWindow.setSize(new Dimension(300,300));
+        appWindow.setTitle("AWT");
+        appWindow.setVisible(true);
+    }
+}
